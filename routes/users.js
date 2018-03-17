b4ddc4bf83ee9cfc77b1edcdd1f06e9f8031faa1
Abdulrahman Desktop <a.alrahama@gmail.com>
2018-03-17 20:18:05 +0300

    naming conventions

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 
 var router = require('express').Router();
-var userService = require('./../modules/services/user');
+var userService = require('./../modules/services/usersService');
 // var token = jwt.sign(user.tokenize(), process.env.SECRET, {expiresIn: tokenTTL});
 
 router.post('/register', userService.register);
