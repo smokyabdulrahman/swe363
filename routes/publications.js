132c4a237b2862998f61e7eff3632611a3f3bba2
ABDULRAHMAN <a.alrahama@gmail.com>
2018-04-21 21:08:44 +0300

    Search doesn't require token

--- routes/publications.js
+++ routes/publications.js
@@ -8,6 +8,6 @@ router.get('/user/:id', authenticateJwt, profilesService.getUserPublications);
 router.post('/', authenticateJwt, profilesService.setPublication);
 router.put('/:id', authenticateJwt, profilesService.updatePublication);
 
-router.get('/search/:keyword', authenticateJwt, profilesService.searchPublication);
+router.get('/search/:keyword', profilesService.searchPublication);
 
 module.exports = router;
