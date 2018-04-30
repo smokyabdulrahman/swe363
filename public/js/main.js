9fe788c0040955967ba5e6965a23c1424c53ddd4
ABDULRAHMAN <a.alrahama@gmail.com>
2018-04-30 17:09:11 +0300

    added cv gen

--- public/js/main.js
+++ public/js/main.js
@@ -56,16 +56,13 @@ function deletePublication(id, userId){
     location.reload();
   })
 }
-// function edit(id){
-//     $.ajax({
-//         type: 'PUT', 
-//         url: '/admins/profiles/edit', 
-//         data: {
-//           userId: id,
-//           newData: 
-//         },
-//       }).done(function(res) {
-//         console.log(res);
-//         location.reload();
-//       })
-// }
+function createCV(){
+  $('#element-to-print').removeAttr('hidden');
+  var element = document.getElementById('element-to-print');
+  html2pdf(element, {
+    margin:       2,
+    filename:     'cv.pdf',
+  });
+  $('#element-to-print').attr('hidden', 'hidden');
+
+}
